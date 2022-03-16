d53f22acd093fcf45caff6276c8967d34bd08712
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-03-16 16:35:13 +0900

    feat: グラフのデータを API で取得してきたものに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PrefectureList from "./component/PrefecturesList";
-import Graph from "./component/graph/Graph";
+import Graph from "./component/Graph";
 
-function App() {
+import axios from "axios";
+
+const App = () => {
+  const [data, setData] = useState<any[]>();
+  useEffect(() => {
+    if (!process.env.REACT_APP_API_KEY) {
+      console.error("environment variables are not set");
+    } else {
+      const axiosConfig = {
+        headers: {
+          "X-API-KEY": process.env.REACT_APP_API_KEY,
+        },
+      };
+
+      const res = axios
+        .get(
+          "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=2&cityCode=-",
+          axiosConfig
+        )
+        .then((response) => {
+          let dataList: any[] = [];
+
+          response.data.result.data[0].data.forEach((data: any) => {
+            dataList.push(data);
+          });
+
+          setData(dataList);
+        });
+    }
+  }, []);
+
   return (
     <>
       <PrefectureList />
-      <Graph />
+      <Graph data={data} />
     </>
   );
-}
+};
 
 export default App;
---
+++ src/component/Graph.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from "recharts";
+
+type Props = {
+  data: any[] | undefined;
+};
+
+const Graph = ({ data }: Props) => {
+  return (
+    <LineChart
+      width={500}
+      height={300}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5,
+      }}
+    >
+      <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      <CartesianGrid stroke="#ccc" />
+      <XAxis dataKey="year" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
+  );
+};
+
+export default Graph;
