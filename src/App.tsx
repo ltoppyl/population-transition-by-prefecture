fe5afaf1ea9cd88cd42471cd3a7f884902d66d51
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-03-16 17:18:28 +0900

    feat: 選択した最後の１つの県の情報がグラフに表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PrefectureList from "./component/PrefecturesList";
 import Graph from "./component/Graph";
 
-import axios from "axios";
-
 const App = () => {
-  const [data, setData] = useState<any[]>();
-  useEffect(() => {
-    if (!process.env.REACT_APP_API_KEY) {
-      console.error("environment variables are not set");
-    } else {
-      const axiosConfig = {
-        headers: {
-          "X-API-KEY": process.env.REACT_APP_API_KEY,
-        },
-      };
-
-      const res = axios
-        .get(
-          "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=2&cityCode=-",
-          axiosConfig
-        )
-        .then((response) => {
-          let dataList: any[] = [];
-
-          response.data.result.data[0].data.forEach((data: any) => {
-            dataList.push(data);
-          });
-
-          setData(dataList);
-        });
-    }
-  }, []);
+  const [graphData, setGraphData] = useState<any[]>();
 
   return (
     <>
-      <PrefectureList />
-      <Graph data={data} />
+      <PrefectureList setGraphData={setGraphData} />
+      <Graph data={graphData} />
     </>
   );
 };
--- src/component/PrefecturesList.tsx
+++ src/component/PrefecturesList.tsx
@@ -1,9 +1,64 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Dispatch, SetStateAction } from "react";
 import axios from "axios";
 import "../style/style.css";
+import { check } from "prettier";
+import { randomInt } from "crypto";
 
-const PrefecturesList = () => {
+type Props = {
+  setGraphData: Dispatch<SetStateAction<any[] | undefined>>;
+};
+
+const PrefecturesList = ({ setGraphData }: Props) => {
   const [prefecturesList, setPrefecturesList] = useState<any[]>();
+  const [checkBoxStatus, setCheckBoxStatus] = useState<boolean[]>([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
 
   useEffect(() => {
     if (!process.env.REACT_APP_API_KEY) {
@@ -24,7 +79,7 @@ const PrefecturesList = () => {
           let prefecturesList: any[] = [];
 
           response.data.result.forEach((data: any) => {
-            prefecturesList.push(data.prefName);
+            prefecturesList.push(data);
           });
 
           setPrefecturesList(prefecturesList);
@@ -32,17 +87,78 @@ const PrefecturesList = () => {
     }
   }, []);
 
+  useEffect(() => {
+    if (!process.env.REACT_APP_API_KEY) {
+      console.error("environment variables are not set");
+    } else {
+      const axiosConfig = {
+        headers: {
+          "X-API-KEY": process.env.REACT_APP_API_KEY,
+        },
+      };
+
+      let checked_number: any[] = [];
+      checkBoxStatus.forEach((check_status, index) => {
+        if (check_status === true) {
+          checked_number.push(index);
+        }
+      });
+
+      const number = checked_number[checked_number.length - 1];
+      console.log(checked_number);
+      console.log(number);
+
+      const res = axios
+        .get(
+          "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=" +
+            number +
+            "&cityCode=-",
+          axiosConfig
+        )
+        .then((response) => {
+          let dataList: any[] = [];
+
+          response.data.result.data[0].data.forEach((data: any) => {
+            dataList.push(data);
+          });
+
+          setGraphData(dataList);
+        });
+    }
+  }, [checkBoxStatus]);
+
+  const handleCheckBox = (data: any) => {
+    setCheckBoxStatus(
+      checkBoxStatus.map((check_statue, index) =>
+        index === data.prefCode - 1 ? !check_statue : check_statue
+      )
+    );
+  };
+
   return (
     <>
       {prefecturesList && (
         <>
-          {prefecturesList.map((prefectures) => {
+          <p>チェックされている県</p>
+          {checkBoxStatus.map((check_status, index) => {
+            if (check_status === true) {
+              return <p key={index}>{prefecturesList[index].prefName}</p>;
+            }
+          })}
+          <p>-------------------------------------</p>
+          {prefecturesList.map((prefecturesData) => {
             return (
-              <div className="prefecturesList" key={prefectures}>
+              <div className="prefecturesList" key={prefecturesData.prefCode}>
                 {/* XXX: なぜ label で囲うとチェックボックスが表示されるか不明 */}
                 <label>
-                  <input key={prefectures} type="checkbox" />
-                  {prefectures}{" "}
+                  <input
+                    key={prefecturesData.prefCode}
+                    type="checkbox"
+                    onClick={() => {
+                      handleCheckBox(prefecturesData);
+                    }}
+                  />
+                  {prefecturesData.prefName}{" "}
                 </label>
               </div>
             );
