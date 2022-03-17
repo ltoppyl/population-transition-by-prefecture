014f5fee388be4969a7773de42293aca994a65bb
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-03-17 17:25:37 +0900

    feat: 機器の判別機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
 import PrefectureList from "./components/PrefecturesList";
 import Graph from "./components/Graph";
 import Title from "./components/Title";
+import { useMediaQueryContext } from "./components/responsive/MediaQueryProvider";
 
 const App = () => {
   const [graphData, setGraphData] = useState<any[]>();
+  const { isMobileSite, isTabletSite, isPcSite } = useMediaQueryContext();
+  const [equipment, setEquipment] = useState<string>("pc");
+
+  useEffect(() => {
+    if (isMobileSite === true) {
+      setEquipment("mobile");
+    } else if (isTabletSite === true) {
+      setEquipment("tablet");
+    } else if (isPcSite === true) {
+      setEquipment("pc");
+    }
+  }, [isMobileSite, isTabletSite, isPcSite]);
 
   return (
     <>
-      <Title />
+      <Title equipment={equipment} />
       <PrefectureList setGraphData={setGraphData} />
       <Graph dataList={graphData} />
     </>
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "../styles/style.css";
 
-const Title = () => {
+type Props = {
+  equipment: string;
+};
+
+const Title = ({ equipment }: Props) => {
   return (
     <>
-      <h1 className="title">都道府県別人口推移</h1>
+      <h1 className="title">都道府県別人口推移 {equipment} ver</h1>
       <h3>※ チェックは最大5個まで！</h3>
     </>
   );
