50ed0783ef497507a9e3c9a839a42f68759d0805
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-03-17 13:07:59 +0900

    feat: 注意書きの追加

--- src/component/Title.tsx
+++ src/component/Title.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import "../style/style.css";
 
 const Title = () => {
-  return <h1 className="title">都道府県別人口推移</h1>;
+  return (
+    <>
+      <h1 className="title">都道府県別人口推移</h1>
+      <h3>※ チェックは最大10個まで！</h3>
+    </>
+  );
 };
 
 export default Title;
