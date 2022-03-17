7795d98da81f826d8bc574fce2f09a0f3ea75ba9
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-03-17 13:03:31 +0900

    feat: タイトルの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import PrefectureList from "./component/PrefecturesList";
 import Graph from "./component/Graph";
+import Title from "./component/Title";
 
 const App = () => {
   const [graphData, setGraphData] = useState<any[]>();
 
   return (
     <>
+      <Title />
       <PrefectureList setGraphData={setGraphData} />
       <Graph dataList={graphData} />
     </>
---
+++ src/component/Title.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../style/style.css";
+
+const Title = () => {
+  return <h1 className="title">都道府県別人口推移</h1>;
+};
+
+export default Title;
