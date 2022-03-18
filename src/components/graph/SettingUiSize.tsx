8b240c4b2b1ce62d587c99c73795b66582632c0d
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-03-18 11:49:58 +0900

    feat: タブレットの場合の UI の設定
    今回は pc と同じであるが、可変とするため

--- src/components/graph/SettingUiSize.tsx
+++ src/components/graph/SettingUiSize.tsx
@@ -3,25 +3,54 @@ import React from "react";
 const SettingUiSize = (equipment: string) => {
   let uiSize;
 
-  if (equipment === "mobile") {
-    uiSize = {
-      width: 350,
-      height: 300,
-      marginTop: 50,
-      marginRight: 20,
-      marginLeft: 20,
-      marginBottom: 0,
-    };
-  } else {
-    uiSize = {
-      width: 700,
-      height: 350,
-      marginTop: 100,
-      marginRight: 35,
-      marginLeft: 20,
-      marginBottom: 0,
-    };
+  switch (equipment) {
+    case "mobile":
+      uiSize = {
+        width: 350,
+        height: 300,
+        marginTop: 50,
+        marginRight: 20,
+        marginLeft: 20,
+        marginBottom: 0,
+        xAxisLabelWidth: 295,
+      };
+      break;
+    case "tablet":
+      uiSize = {
+        width: 700,
+        height: 350,
+        marginTop: 100,
+        marginRight: 35,
+        marginLeft: 20,
+        marginBottom: 0,
+        xAxisLabelWidth: 630,
+      };
+      break;
+    case "pc":
+      uiSize = {
+        width: 700,
+        height: 350,
+        marginTop: 100,
+        marginRight: 35,
+        marginLeft: 20,
+        marginBottom: 0,
+        xAxisLabelWidth: 630,
+      };
+      break;
+    default:
+      uiSize = {
+        width: 700,
+        height: 350,
+        marginTop: 100,
+        marginRight: 35,
+        marginLeft: 20,
+        marginBottom: 0,
+        xAxisLabelWidth: 630,
+      };
+      break;
   }
+
+  console.log(uiSize);
   return uiSize;
 };
 
