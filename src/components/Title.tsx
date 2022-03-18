8fd2c9f2157ef077865e318e19758d2024628967
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2022-03-18 13:17:12 +0900

    refactor: タイトルの不必要になった部分の削除

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import "../styles/style.css";
 
 const Title = () => {
-  return (
-    <>
-      <h1 className="title">都道府県別人口推移</h1>
-    </>
-  );
+  return <h1 className="title">都道府県別人口推移</h1>;
 };
 
 export default Title;
